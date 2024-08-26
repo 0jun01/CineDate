b2bb69148149682c6b8a18690daae2955bc82ae9
0jun01 <dudwns85064843@gmail.com>
2024-08-26 17:08:28 +0900

    api파싱후 박스오피스 메인에 업로드

--- src/main/java/com/tenco/movie/repository/model/Movies.java
+++ src/main/java/com/tenco/movie/repository/model/Movies.java
@@ -19,8 +19,9 @@ public class Movies {
 	private String title; // 영화 제목
 	private String genre; // 영화 장르
 	private String director; // 영화 감독
+	private String movieImg; // 무비 URL주소
 	private String movieDesc; // 영화 상세 설명
-	private Date releaseDate; // 개봉일자
+	private String releaseDate; // 개봉일자
 	private Date endDate; // 종영일자
 	
 }
