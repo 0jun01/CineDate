3447eef1d95b93fcf4897812325930aa22305159
0jun01 <dudwns85064843@gmail.com>
2024-08-27 17:47:40 +0900

    영화 디테일 1차완료

--- src/main/java/com/tenco/movie/controller/MovieController.java
+++ src/main/java/com/tenco/movie/controller/MovieController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tenco.movie.handler.exception.DataDeliveryException;
 import com.tenco.movie.repository.model.Actors;
+<<<<<<< HEAD
 import com.tenco.movie.repository.model.MovieDetail;
+=======
+>>>>>>> 528fecc (영화 디테일 1차완료)
 import com.tenco.movie.repository.model.Movies;
 import com.tenco.movie.service.MovieService;
 import com.tenco.movie.utils.Define;
@@ -20,14 +23,24 @@ import com.tenco.movie.utils.Define;
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
+<<<<<<< HEAD
 
 	private final MovieService movieService;
 
+=======
+	
+	private final MovieService movieService;
+	
+>>>>>>> 528fecc (영화 디테일 1차완료)
 	@Autowired
 	public MovieController(MovieService movieService) {
 		this.movieService = movieService;
 	}
+<<<<<<< HEAD
 
+=======
+	
+>>>>>>> 528fecc (영화 디테일 1차완료)
 	/**
 	 * 영화 페이지 요청
 	 * 
@@ -37,13 +50,18 @@ public class MovieController {
 	public String moviesPage() {
 		return "/movie/moviePage";
 	}
+<<<<<<< HEAD
 
+=======
+	
+>>>>>>> 528fecc (영화 디테일 1차완료)
 	/**
 	 * @param title 박스오피스 영화 제목
 	 * @return 무비디테일 페이지
 	 * @author 변영준
 	 */
 	@GetMapping("/detail")
+<<<<<<< HEAD
 	public String detailPage(Model model, @RequestParam("title") String title) {
 
 		// 타이틀 유효성 검사
@@ -77,4 +95,35 @@ public class MovieController {
 		return "/movie/movieDetail";
 	}
 
+=======
+	public String moviePage(Model model,@RequestParam("title") String title) {
+		
+		if(title == null || title.isEmpty()) {
+			throw new DataDeliveryException(Define.ERROR_INVALID_MOVIE,HttpStatus.BAD_REQUEST);
+		}
+		
+		Movies selectedMovies = movieService.readMovieByTitle(title);
+		
+		if(selectedMovies == null) {
+			throw new DataDeliveryException(Define.ERROR_INVALID_MOVIE, HttpStatus.BAD_REQUEST);
+		}
+		
+		int movieId = selectedMovies.getId();
+		if(movieId == 0) {
+			throw new DataDeliveryException(Define.ERROR_INVALID_MOVIE, HttpStatus.BAD_REQUEST);
+		}
+		System.out.println("--------------");
+		System.out.println(movieId);
+		System.out.println("--------------");
+		List<Actors> movieActors = movieService.readActorsByMovieId(movieId);
+		for (Actors actors : movieActors) {
+			System.out.println(actors);
+		}
+		
+		model.addAttribute("actors",movieActors);
+		model.addAttribute("movie",selectedMovies);
+		return "/movie/movieDetail";
+	}
+	
+>>>>>>> 528fecc (영화 디테일 1차완료)
 }
