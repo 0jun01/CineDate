15c12d11a7b802bfbd6fdb037afb4dfd8d736e48
hyeongjeongyoo <hjeong0711@gmail.com>
2024-09-04 12:46:22 +0900

    이메일 인증 진행 중

--- src/main/java/com/tenco/movie/service/MailSendService.java
+++ src/main/java/com/tenco/movie/service/MailSendService.java
@@ -1,56 +1,43 @@
 package com.tenco.movie.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tenco.movie.handler.exception.DataDeliveryException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class MailSendService {
+	
+	private final JavaMailSender mailSender;
+	
+	public void sendEmail(String toEmail, String title, String text) {
+		SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+		try {
+			mailSender.send(emailForm);
+		} catch (RuntimeException e) {
+			log.debug("MailService.sendEmail exception occur toEmail: {}," + "title: {}, text: {}", toEmail, title, text);
+			throw new DataDeliveryException("이메일 오류", HttpStatus.BAD_REQUEST);
+		}
+	}
 
-	@Autowired
-	private JavaMailSender mailSender;
-	private int authNumber;
-//
-//	// 임의의 6자리 양수를 반환합니다.
-//	public void makeRandomNumber() {
-//		Random random = new Random();
-//		String randomNumber = "";
-//		for (int i = 0; i < 6; i++) {
-//			randomNumber += Integer.toString(random.nextInt(10));
-//		}
-//
-//		authNumber = Integer.parseInt(randomNumber);
-//	}
-//
-//	// mail을 어디서 보내는지, 어디로 보내는지 , 인증 번호를 html 형식으로 어떻게 보내는지 작성합니다.
-//	public String joinEmail(String email) {
-//		makeRandomNumber();
-//		String setFrom = "dionisos198@naver.com"; // email-config에 설정한 자신의 이메일 주소를 입력
-//		String toMail = email;
-//		String title = "회원 가입 인증 이메일 입니다."; // 이메일 제목
-//		String content = "나의 APP을 방문해주셔서 감사합니다." + // html 형식으로 작성 !
-//				"<br><br>" + "인증 번호는 " + authNumber + "입니다." + "<br>" + "인증번호를 제대로 입력해주세요"; // 이메일 내용 삽입
-//		mailSend(setFrom, toMail, title, content);
-//		return Integer.toString(authNumber);
-//	}
-//
-//	// 이메일을 전송합니다.
-//	public void mailSend(String setFrom, String toMail, String title, String content) {
-//		MimeMessage message = mailSender.createMimeMessage();// JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
-//		try {
-//			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");// 이메일 메시지와 관련된 설정을 수행
-//			// true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
-//			helper.setFrom(setFrom);// 이메일의 발신자 주소 설정
-//			helper.setTo(toMail);// 이메일의 수신자 주소 설정
-//			helper.setSubject(title);// 이메일의 제목을 설정
-//			helper.setText(content, true);// 이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정
-//			mailSender.send(message);
-//		} catch (MessagingException e) {// 이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
-//			// 이러한 경우 MessagingException이 발생
-//			e.printStackTrace();// e.printStackTrace()는 예외를 기본 오류 스트림에 출력하는 메서드
-//		}
-//	}
-	
-	
+	// 발신할 이메일 데이터 세팅
+	private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setTo(toEmail);
+		message.setSubject(title);
+		message.setText(text);
+		
+		return message;
+	}
 	
 }
